cd689be36424adf4b96f51030f7c5db50372ddb0
Omar Crosby <omar.crosby@gmail.com>
2024-10-09 22:45:19 -0400

    chore: add tox to the setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
 python3 -m pip install --upgrade pip
-python3 -m pip install setuptools wheel build invoke
+python3 -m pip install setuptools wheel build tox invoke
 invoke install --dev
 
